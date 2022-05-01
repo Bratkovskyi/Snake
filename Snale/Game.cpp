d9765c95835b5a6b9155f1411ac6820aaa61a15d
BratVlad <vlad.bratkovskiy@gmail.com>
2022-05-01 20:42:30 +0300

    Location of things

--- Snale/Game.cpp
+++ Snale/Game.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <conio.h>
 
 using namespace std;
 
@@ -13,10 +14,10 @@ void Setup() {
 	gameOver = false;
 	dir = STOP;
 	x = width / 2;
-	y = height / 2;
+	y = height / 2 ;
 	fruitX = rand() % width;
 	fruitY = rand() % height;
-
+	score = 0;
 }
 
 void Draw() {
@@ -29,7 +30,12 @@ void Draw() {
 		for (int j = 0; j < width; j++) {
 			if (j == 0 || j == width - 1)
 				cout << '#';
-			cout << " ";
+			if (i == y && j == x)
+				cout << "0";
+			else if (i == fruitY && j == fruitX)
+				cout << "F";
+			else
+				cout << " ";
 		} 
 		cout << endl;
 	}
@@ -37,14 +43,57 @@ void Draw() {
 	for (int i = 0; i < width + 1; i++)
 		cout << '#';
 	cout << endl;
+	cout << "Score: " << score << endl;
 }
 
 void Input() {
-
+	if (_kbhit()) {
+		switch (_getch()) 
+		{
+		case 'a':
+			dir = LEFT;
+			break;
+		
+		case 'd':
+			dir = RIGHT;
+			break;
+		case 'w':
+			dir = UP;
+			break;
+		case 's':
+			dir = DOWN;
+			break;
+		case 'x':
+			gameOver = true;
+			break;
+		}
+	}
 }
 
 void Logic() {
+	switch (dir)
+	{
+	case LEFT:
+		x--;
+		break;
+	case RIGHT:
+		x++;
+		break;
+	case  UP:
+		y--;
+		break;
+	case DOWN:
+		y++;
+		break;
+	}
 
+	if (x > width || x < 0 || y > height || y < 0)
+		gameOver = true;
+	if (x == fruitX && y == fruitY) {
+		score += 10;
+		fruitX = rand() % width;
+		fruitY = rand() % height;
+	}
 }
 int main() {
 	Setup();
